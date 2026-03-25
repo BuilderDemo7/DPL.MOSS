c1fc72bef512f5e8663046623c162ec18cfecf8e
BuilderDemo7 <psgameplays245@gmail.com>
2026-03-24 23:50:24 -0300

    added new proj files for input manager, etc.

--- DPL.MOSS/dpl/EInputAction.h
+++ DPL.MOSS/dpl/EInputAction.h
@@ -3,7 +3,9 @@ enum EInputAction
 {
 	EInputAction_Pause = 0,
 	EInputAction_NoController = 1,
+
 	EInputAction_Frontend_Begin = 4,
+
 	EInputAction_Menu_Up = 5,
 	EInputAction_Menu_Down = 6,
 	EInputAction_Menu_Left = 7,
@@ -12,9 +14,15 @@ enum EInputAction
 	EInputAction_Menu_Cancel = 10,
 	EInputAction_Menu_Start = 11,
 
+	EInputAction_Frontend_End = 48,
+	EInputAction_Simulation_Begin = 49,
+
+	EInputAction_Simulation_End = 102,
+
 	EInputAction_Pause_Character_Teleport = 28,
 
 	EInputAction_Visuals_Begin = 103,
+
 	EInputAction_Camera_LookLeft = 108,
 	EInputAction_Camera_LookRight = 109,
 	EInputAction_Camera_LookForward = 110,

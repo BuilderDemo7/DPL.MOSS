7e97fb7f7bc96923da97e67c10e960f731c8197f
BuilderDemo7 <psgameplays245@gmail.com>
2026-03-27 19:21:02 -0300

    modified CGadgetHandler::GetGadgetModelHandle() to use returnStorage

--- DPL.MOSS/dpl/CGadgetHandler.cpp
+++ DPL.MOSS/dpl/CGadgetHandler.cpp
@@ -9,5 +9,6 @@ unsigned short CGadgetHandler::GetGadgetModelHandle(EGadgetType gadget)
 {
 	unsigned int returnStorage = 0;
 
-	return *((unsigned short*(__thiscall*)(CGadgetHandler*, unsigned int*, EGadgetType))0x455c16)(this, &returnStorage, gadget);
+	*((unsigned short*(__thiscall*)(CGadgetHandler*, unsigned int*, EGadgetType))0x455c16)(this, &returnStorage, gadget);
+	return returnStorage;
 }
--- DPL.MOSS/dpl/SObjectiveIcon.h
+++ DPL.MOSS/dpl/SObjectiveIcon.h
@@ -5,14 +5,17 @@
 
 #include "EGadgetType.h"
 
+#pragma pack(push, 1)
 struct SObjectiveIcon
 {
 	Vector4 m_colour;
+
+	int field8; // LocalisedString* m_description
+	
 	CLifeActor* m_pTarget;
 
-	int field8;
-	
-	int m_modelHandle;
+	unsigned short m_modelHandle;
+	short __padding0;
 	int m_activity;
 	int m_displayType;
 	int m_visibility;
@@ -23,13 +26,18 @@ struct SObjectiveIcon
 	bool m_bParticleEffect;
 	void* m_pParticleEffect;
 	unsigned int m_uiEffectIssueNum;
+	char field38;
+
+	char __padding1[3];
+
 	bool m_bUseColour;
 
-	char __padding0[7];
-	
+	char __padding2[3];
+
 	float m_rotation;
 	float m_heightOffset;
 };
+#pragma pack(pop)
 
 void SetupObjectiveIcon(SObjectiveIcon* oicon, int displayType, int lifeEventType = 0);
 void AutoRenderObjectiveIcon(SObjectiveIcon* oicon, Matrix* matrix, CPCViewport* piViewport);
